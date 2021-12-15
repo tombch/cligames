bb589b43eb5738067405da97058fd0bc59d8b11e
tombch <tombrier@hotmail.co.uk>
2021-12-15 00:53:19 +0000

    disentangled menu and updated readme

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Connect4 and Tic-tac-toe on the Command Line
-Text-based versions of Connect4 and Tic-tac-toe. \
-The program contains an implementation of the minimax algorithm (with alpha–beta pruning) for the user to play against, or the user can choose to pit the algorithm against itself. \
-The implementation of the minimax algorithm is contained within the Player class, and is written such that it can play any board games that follow the same structure as the Connect4Board and TictactoeBoard classes. \
-The program is started by running the main.py file. 
+Text-based versions of Connect4 and Tic-tac-toe.
+
+The program contains an implementation of the minimax algorithm (with alpha–beta pruning) for the user to play against, or the user can choose to pit the algorithm against itself.
+
+The implementation of the minimax algorithm is contained within the Player class, and is written such that it can play any board games that follow the same structure as the Connect4Board and TictactoeBoard classes.
+
+The program is started by running `python main.py`. 
 ## How to play
-* To make a move in Tic-tac-toe, input 'xy' when prompted, where 'x' and 'y' are the x and y coordinates of a square on the board.
-* To make a move in Connect4, enter 'x' when prompted, where 'x' is the index of the column you wish to make a move in.
+#### Tic-tac-toe
+To make a move in Tic-tac-toe, input `xy` when prompted, where `x` and `y` are the x and y coordinates of a square on the board.
+#### Connect4
+To make a move in Connect4, enter `x` when prompted, where `x` is the index of the column you wish to make a move in.
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-import time
+from timeit import default_timer as timer
 import connect4_board
 import tictactoe_board
 import player
 
-def custom_quit():
-    print("Quitting the program.")
-    quit()
 
-def invalid_choice():
-    print("Not a valid choice.")
+messages = {
+    'game_options' : '1: Connect4\n  2: Tic-tac-toe\n  3: Exit',
+    'player_mode_options' : '1: Player vs Player\n  2: Player vs Computer\n  3: Computer vs Computer\n  4: Choose a different game\n  5: Exit',
+    'choose_piece' : 'Do you want to play as X or O against the computer? X has the first move.',
+    'replay_options' : '1: Play again\n  2: Change who plays the game\n  3: Choose a different game\n  4: Exit',
+    'invalid' : 'Not a valid choice.',
+    'quit' : 'Exiting the program.'
+}
 
-def choose_replay_options():
-        print("Please choose one of the following options listed below."
-        "\n  1: Play again\n  2: Change who plays the game\n  3: Choose a different game\n  4: Exit")
+
+def choose_replay(player_X, player_O, board, display_options=True):
+    if display_options:
+        print('Please choose one of the following options listed below.')
+        print(f"  {messages['replay_options']}")
+    choice = input("Enter your choice here: ")
+    # Replay game
+    if choice == '1':
+        play_game(player_X, player_O, board)
+    # Change who plays the game
+    elif choice == '2':
+        choose_player_mode(board)
+    # Choose a different game
+    elif choice == '3':
+        choose_game()
+    # Quit program
+    elif choice == '4':
+        print(messages['quit'])
+        quit()
+    # Invalid choice
+    else:
+        print(messages['invalid'])
+        choose_replay(player_X, player_O, board, display_options=False)
+
 
 def play_game(player_X, player_O, board):
     board.reset_board()
@@ -21,13 +45,13 @@ def play_game(player_X, player_O, board):
     board.print_board()   
     current_player = player_X
     while winner == "":
-        initial_time = time.time()
+        start = timer()
         current_player_move = current_player.move(board)
-        decision_time = time.time() - initial_time
+        end = timer()
         board.place_disc(current_player.disc, current_player_move, "game")    
         board.print_board()
-        print("Player " + current_player.disc + " chose move " + str(current_player_move) + ".")
-        print("Decision time:", (decision_time), "seconds")
+        print(f"Player {current_player.disc} chose move {current_player_move}.")
+        print(f"Decision time: {end - start} seconds")
         if board.check_for_win(current_player.disc):
             winner = current_player.disc
         elif board.board_full():
@@ -40,92 +64,83 @@ def play_game(player_X, player_O, board):
     if winner == "draw":
         print("Game ended in a draw.")
     else:
-        print("Player " + winner + " wins! Game Over.")
-    choose_replay_options()
-    choosing_replay = True
-    while choosing_replay:
-        replay_choice = input("Enter your choice here: ")
-        if replay_choice == '1':
-            play_game(player_X, player_O, board)
-        elif replay_choice == '2':
-            choosing_replay = False
-        elif replay_choice == '3':
-            choose_game()
-        elif replay_choice == '4':
-            custom_quit()
-        else:
-            invalid_choice()
-    
-def choose_piece():
+        print(f"Player {winner} wins! Game Over.")
+    choose_replay(player_X, player_O, board)
+
+
+def choose_piece(display_options=True):
+    if display_options:
+        print(messages['choose_piece'])
     choosing_piece = True
-    piece_choice = ""
     while choosing_piece:
-        piece_choice = input("Do you want to play as X or O against the computer? X has the first move. Enter your choice here: ")
-        if piece_choice.upper() == 'X' or piece_choice.upper() == 'O':
+        choice = input("Enter your choice here: ").strip().upper()
+        if choice == 'X' or choice == 'O':
             choosing_piece = False
         else:
-            invalid_choice()
-    return piece_choice
+            print(messages['invalid'])
+    return choice
 
-def choose_mode_options():
-    print("Please choose one of the following options listed below."
-    "\n  1: Player vs Player\n  2: Player vs Computer\n  3: Computer vs Computer\n  4: Choose a different game\n  5: Exit")
 
-def choose_mode(board):
-    choose_mode_options()
-    choosing_mode = True
-    while choosing_mode:
-        mode_choice = input("Enter your choice here: ")
-        if mode_choice == '1':
-            play_game(player.Player("human", "X"), player.Player("human", "O"), board)
-            choose_mode_options()
-        elif mode_choice == '2':
-            chosen_piece = choose_piece()
-            if chosen_piece.upper() == 'X':
-                play_game(player.Player("human", "X"), player.Player("computer", "O"), board)
-                choose_mode_options()
-            elif chosen_piece.upper() == 'O':
-                play_game(player.Player("computer", "X"), player.Player("human", "O"), board)
-                choose_mode_options()
-        elif mode_choice == '3':
-            play_game(player.Player("computer", "X"), player.Player("computer", "O"), board)
-            choose_mode_options()
-        elif mode_choice == '4':
-            choosing_mode = False
-        elif mode_choice == '5':
-            custom_quit()
-        else:
-            invalid_choice()
+def choose_player_mode(board, display_options=True):
+    if display_options:
+        print('Please choose one of the following options listed below.')
+        print(f"  {messages['player_mode_options']}")
+    choice = input("Enter your choice here: ")
+    # Human vs Human
+    if choice == '1':
+        play_game(player.Player("human", "X"), player.Player("human", "O"), board)
+    # Human vs Computer
+    elif choice == '2':
+        chosen_piece = choose_piece()
+        if chosen_piece.upper() == 'X':
+            play_game(player.Player("human", "X"), player.Player("computer", "O"), board)
+        elif chosen_piece.upper() == 'O':
+            play_game(player.Player("computer", "X"), player.Player("human", "O"), board)
+    # Robot wars
+    elif choice == '3':
+        play_game(player.Player("computer", "X"), player.Player("computer", "O"), board)
+    # Different game
+    elif choice == '4':
+        choose_game()
+    # Quit program
+    elif choice == '5':
+        print(messages['quit'])
+        quit()
+    # Invalid choice
+    else:
+        print(messages['invalid'])
+        choose_player_mode(board, display_options=False)
 
-def choose_game_options():
-    print("Please choose one of the following options listed below."
-    "\n  1: Connect4\n  2: Tic-tac-toe\n  3: Exit")
 
-def choose_game():   
-    choose_game_options()
-    choosing_game = True
-    while choosing_game:
-        game_choice = input("Enter your choice here: ")
-        if game_choice == '1':
-            print("LETS PLAY CONNECT4.")
-            board = connect4_board.Connect4Board()
-            choose_mode(board)
-            choose_game_options()
-        elif game_choice == '2':
-            print("LETS PLAY TIC-TAC-TOE.")
-            board = tictactoe_board.TictactoeBoard()
-            choose_mode(board)
-            choose_game_options()
-        elif game_choice == '3':
-            choosing_game = False
-        else:
-            invalid_choice()
-    custom_quit()
+def choose_game(display_options=True):
+    if display_options:
+        print('Please choose one of the following options listed below.')  
+        print(f"  {messages['game_options']}")
+    choice = input("Enter your choice here: ")
+    # Connect4
+    if choice == '1':
+        print("LETS PLAY CONNECT4.")
+        board = connect4_board.Connect4Board()
+        choose_player_mode(board)
+    # Tic-tac-toe
+    elif choice == '2':
+        print("LETS PLAY TIC-TAC-TOE.")
+        board = tictactoe_board.TictactoeBoard()
+        choose_player_mode(board)
+    # Quit program
+    elif choice == '3':
+        print(messages['quit'])
+        quit()
+    # Invalid choice
+    else:
+        print(messages['invalid'])
+        choose_game(display_options=False)
+
 
 def main():
     print("Welcome to Command Line Board Games.") 
     choose_game()
 
+
 if __name__ == '__main__':
-    main()
-    
+    main()    
